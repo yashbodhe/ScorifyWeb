038fc8369b9613f2da4803d91941646f6fb1c423
yashbodhe <71684249+yashbodhe@users.noreply.github.com>
2021-02-20 21:04:09 +0530

    second commit

--- Scorify/views.py
+++ Scorify/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("<h1>Hello Yash, Welcome to Scorify Web</h1>")
+	return render(request,'index.html')
