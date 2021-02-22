a8f394e9e0e522e4a1b0f3d659408a7c23e94f74
yashbodhe <71684249+yashbodhe@users.noreply.github.com>
2021-02-22 13:39:53 +0530

    trying pickle

--- Scorify/views.py
+++ Scorify/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from Scorify.teams import createTeam,storeData,loadData
 def index(request):
-	return render(request,'index.html')
+	teamObj=createTeam()
+	storeData(teamObj)
+	increOne()
+	increOne()
+	increOne()
+	teamObj=loadData()
+	param={'team':teamObj}
+	return render(request,'index.html',param)
+def increOne():
+	obj=loadData()
+	obj.score+=1
+	storeData(obj)
+	
